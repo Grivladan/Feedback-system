24e6a113b6cd96eeed813d00ea02fcd4a6744d69
Vladyslav Hryhorenko <grigorenkovlad1993@gmail.com>
2016-10-23 01:40:02 +0300

    add dependency injection for data acesss layer

---
+++ FeedbackSystem.DataAccess/Infrastructure/DataAccessModule.cs
@@ -0,0 +1,19 @@
+﻿using FeedbackSystem.DataAccess.EF;
+using FeedbackSystem.DataAccess.Interfaces;
+using FeedbackSystem.DataAccess.Repository;
+using Ninject.Extensions.Factory;
+using Ninject.Modules;
+
+namespace FeedbackSystem.DataAccess.Infrastructure
+{
+    public class DataAccessModule : NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<IRepositoryFactory>().ToFactory();
+            Bind<ApplicationDbContext>().ToSelf();
+            Bind(typeof(IRepository<>)).To(typeof(Repository<>));
+            Bind<IUnitOfWork>().To<EFUnitOfWork>();
+        }
+    }
+}
